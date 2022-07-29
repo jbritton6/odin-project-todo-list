c987788eed8959872ee3d33fb02b7bd3384c93ba
Jacob Britton <jacobbritton95@gmail.com>
2022-07-29 11:41:17 -0400

    Link css and get basic test working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert('hello world!')
+import './styles/styles.css'
+
+
+main();
+
+function main() {
+    const content = document.querySelector('div#content');
+
+    content.appendChild(board());
+}
+
+function board() {
+    const element = document.createElement('div');
+    element.classList.add('board');
+
+    return element;
+}
