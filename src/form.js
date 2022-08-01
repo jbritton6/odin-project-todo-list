b91ac1e309686baacff0d0ca90d8d44a572183f3
Jacob Britton <jacobbritton95@gmail.com>
2022-07-31 21:10:47 -0400

    Add due date form element

--- src/form.js
+++ src/form.js
@@ -17,6 +17,7 @@ function createForm() {
     form.appendChild(title);
     form.appendChild(createName());
     form.appendChild(createDescription());
+    form.appendChild(createDueDate());
 
     return form;
 }
@@ -56,6 +57,26 @@ function createDescription() {
     container.classList.add('form-element');
     container.appendChild(descriptionLabel);
     container.appendChild(descriptionInput);
+
+    return container;
+}
+
+
+function createDueDate() {
+    const dueDateLabel = document.createElement('lable');
+    dueDateLabel.classList.add('form-label');
+    dueDateLabel.setAttribute('for', 'due-date');
+    dueDateLabel.textContent = 'Due Date';
+
+    const dueDateInput = document.createElement('input');
+    dueDateInput.setAttribute('id', 'due-date');
+    dueDateInput.setAttribute('type', 'date');
+    dueDateInput.setAttribute('required', 'required');
+
+    const container = document.createElement('div');
+    container.classList.add('form-element');
+    container.appendChild(dueDateLabel);
+    container.appendChild(dueDateInput);
 
     return container;
 }
