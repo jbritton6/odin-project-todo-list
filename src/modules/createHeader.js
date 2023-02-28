6c655755b70e949b44cbf63fd89db603a98da280
Jacob Britton <jacobbritton95@gmail.com>
2023-02-28 13:25:28 -0500

    Generate app title

--- src/modules/createHeader.js
+++ src/modules/createHeader.js
@@ -1,8 +1,17 @@
 import '../styles/styles.css'
 
+function createTitle(title) {
+    const titleContainer = document.createElement('h1');
+    titleContainer.textContent = title;
+
+    return titleContainer;
+}
+
 function createHeader() {
     const header = document.createElement('div');
     header.classList.add('header');
+
+    header.appendChild(createTitle('Todo List'));
 
     return header;
 }
