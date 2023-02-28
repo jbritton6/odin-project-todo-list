85635da6ac40257a9f928d5d1c93a6b846e7195f
Jacob Britton <jacobbritton95@gmail.com>
2023-02-28 14:23:02 -0500

    Create radio button priority input

--- src/modules/createModal.js
+++ src/modules/createModal.js
@@ -41,12 +41,48 @@ function createDateInput(labelTitle, identifier) {
     return container;
 }
 
+function createPriorityInput() {
+    const legend = document.createElement('legend');
+    legend.textContent = "Priority";
+
+    const low = createRadioInput('Low:', 'low', 'priority', 'low');
+    const medium = createRadioInput('Medium:', 'medium', 'priority', 'medium');
+    const high = createRadioInput('High:', 'high', 'priority', 'high');
+
+    const fieldset = document.createElement('fieldset');
+    fieldset.appendChild(legend);
+    fieldset.appendChild(low);
+    fieldset.appendChild(medium);
+    fieldset.appendChild(high);
+
+    return fieldset;
+}
+
+function createRadioInput(labelTitle, identifier, group, value) {
+    const label = document.createElement('label');
+    label.setAttribute('for', identifier);
+    label.textContent = labelTitle;
+
+    const input = document.createElement('input');
+    input.setAttribute('type', 'radio');
+    input.setAttribute('id', identifier);
+    input.setAttribute('name', group);
+    input.setAttribute('value', value);
+
+    const container = document.createElement('div');
+    container.appendChild(label);
+    container.appendChild(input);
+
+    return container;
+}
+
 function createModalForm() {
     const form = document.createElement('form');
 
     form.appendChild(createTextInput('Title:', 'title'));
     form.appendChild(createTextInput('Project:', 'project'));
     form.appendChild(createDateInput('Due date:', 'due-date'))
+    form.appendChild(createPriorityInput());
 
     return form;
 }
