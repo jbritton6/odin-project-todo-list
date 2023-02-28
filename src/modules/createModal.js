3a84144845c2f110fcfe230347e5827973ec8104
Jacob Britton <jacobbritton95@gmail.com>
2023-02-28 13:58:09 -0500

    Implement basic form text input

--- src/modules/createModal.js
+++ src/modules/createModal.js
@@ -7,11 +7,31 @@ function createModalBackground() {
     return modalBackground;
 }
 
+
+function createModalForm() {
+    const form = document.createElement('form');
+
+    const title = document.createElement('label');
+    title.setAttribute('for', 'title');
+    title.textContent = 'Title:';
+    const input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', 'title');
+    input.setAttribute('name', 'title');
+
+    form.appendChild(title);
+    form.appendChild(input);
+
+    return form;
+}
+
 function createModal() {
     const modalBackground = createModalBackground();
+    const modalForm = createModalForm();
 
     const modal = document.createElement('div');
 
+    modalBackground.appendChild(modalForm);
     modal.appendChild(modalBackground);
 
     return modal;
