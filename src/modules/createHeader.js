1d5af77b6c55cee87ee7b43bb94460695fe684a3
Jacob Britton <jacobbritton95@gmail.com>
2023-02-28 13:29:00 -0500

    Add button to create new todo

--- src/modules/createHeader.js
+++ src/modules/createHeader.js
@@ -7,11 +7,20 @@ function createTitle(title) {
     return titleContainer;
 }
 
+function createAddTodoButton(text) {
+    const addTodoButton = document.createElement('button');
+    addTodoButton.classList.add('add-todo-button');
+    addTodoButton.textContent = text;
+
+    return addTodoButton;
+}
+
 function createHeader() {
     const header = document.createElement('div');
     header.classList.add('header');
 
     header.appendChild(createTitle('Todo List'));
+    header.appendChild(createAddTodoButton('+'));
 
     return header;
 }
