4321792cbdc73be441667e42a59e60d513ca53c5
Jacob Britton <jacobbritton95@gmail.com>
2023-03-02 13:04:00 -0500

    Refactor button toggle functionality

--- src/modules/createHeader.js
+++ src/modules/createHeader.js
@@ -7,17 +7,14 @@ function createTitle(title) {
     return titleContainer;
 }
 
-function toggleDisplay() {
-    console.log('hu');
-    const element = document.querySelector('.modal-background');
-    element.classList.toggle('hide-display');
-}
-
 function createAddTodoButton(text) {
     const addTodoButton = document.createElement('button');
     addTodoButton.classList.add('add-todo-button');
     addTodoButton.textContent = text;
-    addTodoButton.addEventListener('click', toggleDisplay);
+    addTodoButton.addEventListener('click', () => {
+        const element = document.querySelector('.modal-background');
+        element.classList.toggle('hide-display');
+    });
 
     return addTodoButton;
 }
